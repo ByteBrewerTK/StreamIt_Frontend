36f8cd9aa3fb2e28eb5ce1ef33cfdfed72b40518
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-06 20:35:03 +0530

    setup - project files

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				accent: "var(--color-accent)",
+				muted: "var(--color-muted)",
+				muted_border: "var(--color-border-muted)",
+			},
+
+			width: {
+				container: "var(--width-container)",
+			},
+			height: {
+				container: "var(--height-container)",
+			},
+			fontSize: {
+				smr: "var(--font-small)",
+				mid: "var(--font-normal)",
+				lrg: "var(--font-large)",
+				lgr: "var(--font-larger)",
+				lgt: "var(--font-largest)",
+				mv: "var(--font-massive)",
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
