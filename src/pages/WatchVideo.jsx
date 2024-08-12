39364f6b725612bf5aaf1c8b49dbdd0af41ad540
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-12 23:08:01 +0530

    update - watch video ui component

--- src/pages/WatchVideo.jsx
+++ src/pages/WatchVideo.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
-import { apiRequest } from "../services/api";
+import { apiRequest, toggleLikeApi } from "../services/api";
 import { useSearchParams, Link } from "react-router-dom";
+import { LuThumbsUp } from "react-icons/lu";
+import { RiShareForwardLine } from "react-icons/ri";
+import { IoBookmarkOutline } from "react-icons/io5";
+import Comments from "../components/video/Comments";
 
 const WatchVideo = () => {
 	const [searchParams] = useSearchParams();
@@ -30,22 +34,26 @@ const WatchVideo = () => {
 		fetchData();
 	}, [videoId]);
 
+	
 	const videoPlayerStyle = {
 		width: "100%",
 		height: "100%",
 	};
-
+	
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>{error}</p>;
 	if (!results || !results.ownerDetails)
 		return <p>No video details available</p>;
-
+	
+	const toggleLike = async ()=>{
+		await toggleLikeApi("Video", videoId);
+	}
 	const { ownerDetails } = results;
 
 	return (
-		<div className="px-4 text-white">
+		<div className="w-full text-white lg:px-4">
 			<section>
-				<div className="w-[70rem] aspect-video rounded-lg overflow-hidden bg-black">
+				<div className=" lg:w-[70rem] aspect-video rounded-lg overflow-hidden bg-black">
 					<ReactPlayer
 						url={results.videoFile}
 						controls={true}
@@ -55,14 +63,16 @@ const WatchVideo = () => {
 					/>
 				</div>
 
-				<div>
-					<div>
-						<h2>{results.title}</h2>
-						<div>
-							<div className="flex">
+				<div className="px-2 space-y-2 border border-red-500 md:space-y-4">
+					<h2 className="font-semibold md:text-mid">
+						{results.title}
+					</h2>
+					<div className="flex flex-col items-center justify-between md:flex-row gap-y-4">
+						<div className="flex items-center justify-between w-full md:space-x-4">
+							<div className="flex items-center space-x-2">
 								<Link
 									to={`/user/${ownerDetails._id}`}
-									className="overflow-hidden w-[50px] aspect-square rounded-full"
+									className="overflow-hidden w-[35px] aspect-square rounded-full h-[35px] md:w-[45px] md:h-auto"
 								>
 									{/* Avatar */}
 									<img
@@ -71,38 +81,70 @@ const WatchVideo = () => {
 											ownerDetails.fullName ||
 											"Channel Avatar"
 										}
-										width={50}
-										height={50}
+										width={45}
+										height={45}
 										loading="lazy"
 									/>
 								</Link>
-								<div>
+								<div className="flex items-center gap-2">
 									{/* Channel name and subscribers */}
-									<Link to={`/user/${ownerDetails._id}`}>
+									<Link
+										to={`/user/${ownerDetails._id}`}
+										className="font-[500] text-sm"
+									>
 										{ownerDetails.fullName}
 									</Link>
-									<p>
+									<p className="inline-block text-smr text-muted_dark">
 										{ownerDetails.subscribersCount}{" "}
-										subscribers
+										<span className="hidden md:inline-block">
+											subscribers
+										</span>
 									</p>
-								</div>
-								<div>
-									{/* Subscribed button */}
-									<button className="px-2 py-1 text-black bg-white rounded-full">
-										{ownerDetails.isSubscribed
-											? "Subscribed"
-											: "Subscribe"}
-									</button>
 								</div>
 							</div>
 							<div>
-								<div>{/* like button */}</div>
-								<button>{/* share button */}</button>
-								<button>{/* save */}</button>
+								{/* Subscribed button */}
+								<button className="px-3 py-1 text-black bg-white rounded-full text-smr md:px-4">
+									{ownerDetails.isSubscribed
+										? "Subscribed"
+										: "Subscribe"}
+								</button>
+							</div>
+						</div>
+						<div className="w-full overflow-x-auto overflow-y-hidden">
+							<div className="flex items-center space-x-2">
+								<button
+									className="flex items-center px-6 py-1 space-x-2 text-sm border rounded-full border-muted"
+									onClick={toggleLike}
+								>
+									{/* like button */}
+									<LuThumbsUp className="text-sm " />
+									<span>{results.likes}</span>
+								</button>
+								<button className="flex items-center px-4 py-1 space-x-2 text-sm border rounded-full border-muted">
+									{/* share button */}
+									<RiShareForwardLine className="text-sm " />
+									<span>Share</span>
+								</button>
+								<button className="flex items-center px-4 py-1 space-x-2 text-sm border rounded-full border-muted">
+									{/* save */}
+									<IoBookmarkOutline className="text-sm " />
+									<span>Save</span>
+								</button>
 							</div>
 						</div>
 					</div>
-					<div></div>
+					<div>
+						<div>
+							<div className="flex space-x-2">
+								<span>{results.views} views</span>
+								<span></span>
+							</div>
+
+							<div>{results.description}</div>
+						</div>
+					</div>
+					<Comments />
 				</div>
 			</section>
 			<div></div>
