78e2005a52266d52afa78a9317f7f56dff9df320
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-14 16:11:25 +0530

    update - add algo for views and count formatting

--- src/components/video/VideoItem.jsx
+++ src/components/video/VideoItem.jsx
@@ -1,5 +1,7 @@
 import { IoMdMore } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { getTimeAgo } from "../../utils/getTimeAgo";
+import { formatCounts } from "../../utils/formatCounts";
 const VideoItem = (element) => {
 	let duration = `${(element.duration / 60).toFixed(2)}`.replace(".", ":");
 	return (
@@ -35,18 +37,21 @@ const VideoItem = (element) => {
 							{element.title}
 						</h3>
 						<div className="text-[0.7rem] flex flex-row gap-2 text-gray-400  md:flex-col md:gap-0 ">
-							<Link to={`/channel/${element.username}`} className="transition-all md:text-smr hover:text-gray-100">
-								{element.fullName}
+							<Link
+								to={`/channel/${element.username}`}
+								className="transition-all md:text-smr hover:text-gray-100"
+							>
+								{element.channelName}
 							</Link>
 							<div className="space-x-2">
-								<span>{element.views} views</span>
-								<span>{element.createAt}</span>
+								<span>{formatCounts(element.views)} </span>
+								<span>{getTimeAgo(element.createdAt)}</span>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div>
-					<IoMdMore className="text-[1.5rem]" />
+					<IoMdMore className="text-[1.5rem] text-white" />
 				</div>
 			</div>
 		</Link>
