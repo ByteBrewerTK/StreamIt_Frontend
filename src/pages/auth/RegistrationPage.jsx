2873d3fcf7df4eb71d5a040ee55ec12c72c80a80
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-16 17:28:11 +0530

    update - registration api request

--- src/pages/auth/RegistrationPage.jsx
+++ src/pages/auth/RegistrationPage.jsx
@@ -11,36 +11,71 @@ import OauthUi from "../../components/OauthUi";
 import { Link } from "react-router-dom";
 import FormHeader from "../../components/ui/FormHeader";
 import CTAButton from "../../components/ui/CTAButton";
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo.png";
+import { apiInstance } from "../../services/api";
+import Loader from "../../components/ui/loader/Loader";
 const RegistrationPage = () => {
+	const [isLoading, setLoading] = useState(false);
 	const [isPassVissible, setPassVissible] = useState(false);
+	const [formData, setFormData] = useState({
+		fullName: "",
+		email: "",
+		password: "",
+	});
 
+	const formHandler = (e) => {
+		const { name, value } = e.target;
+		setFormData((prevData) => ({
+			...prevData,
+			[name]: value,
+		}));
+	};
+	
 	const handleToggle = () => {
 		setPassVissible(!isPassVissible);
+	};
+	
+	const submitHandler = async (e) => {
+		e.preventDefault();
+		setLoading(true)
+		try {
+			const response = await apiInstance.post("/user/register", formData);
+			console.log(response);
+			setLoading(false)
+		} catch (error) {
+			console.log("Error occurred while registering user : ", error);
+			setLoading(false)
+		}
 	};
 
 	const form_header = {
 		heading: "Welcome",
 		description:
-			"Complete the registration form for exclusive features and personalized content.",
+		"Complete the registration form for exclusive features and personalized content.",
 	};
 	return (
 		<main className="grid w-full h-full place-items-center">
 			<div className="space-y-2 w-container h-fit sm:w-[360px]  sm:p-4 sm:rounded-2xl sm:shadow-lg sm:py-8">
-				<FormHeader {...form_header} >
-					<img src={logo} alt="" width={60} height={60}/>
+				<FormHeader {...form_header}>
+					<img src={logo} alt="" width={60} height={60} />
 				</FormHeader>
 
 				<div className="space-y-4">
-					<form action="" className="flex flex-col space-y-2">
+					<form
+						onSubmit={submitHandler}
+						className="flex flex-col space-y-2"
+					>
 						<label className="flex items-center justify-between w-full px-4 py-2 space-x-2 border-[1.5px] rounded-full border-muted_border focus-within:border-black">
 							<span className="text-muted">
 								<FaRegEnvelope />
 							</span>
 							<input
 								type="email"
+								name="email"
 								className="w-full outline-none placeholder:text-muted placeholder:text-smr placeholder:select-none"
 								placeholder="Enter your email"
+								onChange={formHandler}
+								required
 							/>
 						</label>
 						<label className="flex items-center justify-between w-full px-4 py-2 space-x-2 border-[1.5px] rounded-full border-muted_border focus-within:border-black">
@@ -48,9 +83,12 @@ const RegistrationPage = () => {
 								<FaRegUser />
 							</span>
 							<input
-								type="username"
+								type="fullName"
+								name="fullName"
 								className="w-full outline-none placeholder:text-muted placeholder:text-smr placeholder:select-none"
-								placeholder="Enter a username"
+								placeholder="Enter your full name"
+								onChange={formHandler}
+								required
 							/>
 						</label>
 						<label className="flex items-center justify-between w-full px-4 py-2 space-x-2 border-[1.5px] rounded-full border-muted_border focus-within:border-black">
@@ -59,8 +97,11 @@ const RegistrationPage = () => {
 							</span>
 							<input
 								type={isPassVissible ? "text" : "password"}
+								name="password"
 								className="w-full outline-none placeholder:text-muted placeholder:text-smr placeholder:select-none "
 								placeholder="Enter new password"
+								onChange={formHandler}
+								required
 							/>
 							<span
 								className="cursor-pointer text-muted"
@@ -74,7 +115,17 @@ const RegistrationPage = () => {
 							</span>
 						</label>
 
-						<CTAButton>Sign up</CTAButton>
+						<CTAButton disabled={isLoading}>
+							{!isLoading ? (
+								"Sign up"
+							) : (
+								<span className="flex items-center justify-center w-full h-full">
+									<span className="w-[1.3rem] aspect-square">
+										<Loader />
+									</span>
+								</span>
+							)}
+						</CTAButton>
 					</form>
 
 					<OauthUi />
