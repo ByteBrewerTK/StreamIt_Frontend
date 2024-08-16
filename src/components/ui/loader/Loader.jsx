ef70aca91994588cc4d70c590fb7e9e92bf0f623
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-16 17:34:12 +0530

    remove - css import

--- src/components/ui/loader/Loader.jsx
+++ src/components/ui/loader/Loader.jsx
@@ -1,6 +1,7 @@
-import "./loader.css"
 const Loader = () => {
-	return <span className="loader"></span>;
+	return (
+		<span className="animate-spin inline-block w-full border-[2px] border-current border-t-transparent text-muted rounded-full h-full  dark:text-muted "></span>
+	);
 };
 
 export default Loader;
