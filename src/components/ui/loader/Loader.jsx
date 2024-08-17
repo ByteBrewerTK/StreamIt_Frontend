82fd0973365dda7abfe095fcd75bd7d67768a9b0
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-17 23:19:23 +0530

    update - ui loader

--- src/components/ui/loader/Loader.jsx
+++ src/components/ui/loader/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
 	return (
-		<span className="animate-spin inline-block w-full border-[2px] border-current border-t-transparent text-muted rounded-full h-full  dark:text-muted "></span>
+		<span className="animate-spin inline-block w-full border-[2px] border-current border-t-transparent text-muted rounded-full h-full dark:text-muted "></span>
 	);
 };
 
