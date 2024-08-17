68d1a947b5a8aa416c5fff21085da3860f6bad9a
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-17 23:21:04 +0530

    add - react toaster

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PrivateRoutes from "./pages/auth/PrivateRoutes";
 import VerificationSuccess from "./pages/auth/VerificationSuccess";
 import ResendVerificationMail from "./pages/auth/ResendVerificationMail";
 import Loader from "./components/ui/loader/Loader";
+import {Toaster} from 'react-hot-toast';
 
 // Lazy load the component
 const LoginPage = lazy(() => import("./pages/auth/LoginPage"));
@@ -16,48 +17,54 @@ const WatchVideo = lazy(() => import("./pages/WatchVideo"));
 
 function App() {
 	return (
-		<div className="h-[calc(100dvh)] w-full font-poppins overflow-hidden">
-			<Suspense
-				fallback={
-					<div className="grid w-full h-full place-items-center bg-secondary">
-						{" "}
-						<span className=" size-14">
-							<Loader />
-						</span>
-					</div>
-				}
-			>
-				<Routes>
-					<Route path="/auth/login" element={<LoginPage />} />
-					<Route
-						path="/auth/sign-up"
-						element={<RegistrationPage />}
-					/>
-					<Route
-						path="/auth/reset-password"
-						element={<PasswordResetPage />}
-					/>
-					<Route path="/auth/verify-otp" element={<VerifyOTP />} />
-					<Route
-						path="/auth/new-password"
-						element={<CreateNewPassword />}
-					/>
-					<Route
-						path="/confirm/:token"
-						element={<VerificationSuccess />}
-					/>
-					<Route
-						path="/resend/confirm/:email"
-						element={<ResendVerificationMail />}
-					/>
+		<>
+			<Toaster position="top-center" reverseOrder={false} />
+			<div className="h-[calc(100dvh)] w-full font-poppins overflow-hidden">
+				<Suspense
+					fallback={
+						<div className="grid w-full h-full place-items-center bg-secondary">
+							{" "}
+							<span className=" size-14">
+								<Loader />
+							</span>
+						</div>
+					}
+				>
+					<Routes>
+						<Route path="/auth/login" element={<LoginPage />} />
+						<Route
+							path="/auth/sign-up"
+							element={<RegistrationPage />}
+						/>
+						<Route
+							path="/auth/reset-password"
+							element={<PasswordResetPage />}
+						/>
+						<Route
+							path="/auth/verify-otp"
+							element={<VerifyOTP />}
+						/>
+						<Route
+							path="/auth/new-password"
+							element={<CreateNewPassword />}
+						/>
+						<Route
+							path="/confirm/:token"
+							element={<VerificationSuccess />}
+						/>
+						<Route
+							path="/resend/confirm/:email"
+							element={<ResendVerificationMail />}
+						/>
 
-					<Route element={<PrivateRoutes />}>
-						<Route path="/" element={<FeedsPage />} />
-						<Route path="/watch" element={<WatchVideo />} />
-					</Route>
-				</Routes>
-			</Suspense>
-		</div>
+						<Route element={<PrivateRoutes />}>
+							<Route path="/" element={<FeedsPage />} />
+							<Route path="/watch" element={<WatchVideo />} />
+						</Route>
+					</Routes>
+				</Suspense>
+			</div>
+		</>
 	);
 }
 
