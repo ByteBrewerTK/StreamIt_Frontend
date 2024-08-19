2576bb9cb22ba6fb7026f59279f52ddac1e3258c
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-19 10:50:59 +0530

    update - response if data is not available

--- src/pages/FeedsPage.jsx
+++ src/pages/FeedsPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { apiRequest } from "../services/api";
 import { useState } from "react";
 import VideoItem from "../components/video/VideoItem";
+import Loader from "../components/ui/loader/Loader";
 
 const FeedsPage = () => {
 	const [data, setData] = useState([]);
@@ -14,7 +15,6 @@ const FeedsPage = () => {
 		sortType: "asc",
 	};
 	const url = `/video?limit=${query.limit}&page=${query.page}&sortBy=${query.sortBy}&sortType=${query.sortType}`;
-
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -31,13 +31,27 @@ const FeedsPage = () => {
 		};
 		fetchData();
 	}, []);
-	// if (!data) return <p>Loading...</p>;
+	if (!data)
+		return (
+			<main className="grid w-full h-full place-items-center">
+				<div className="size-[60px]">
+					
+					<Loader />
+				</div>
+			</main>
+		);
+	if(!data.totalDocs)return (
+		<main className="grid w-full h-full place-items-center">
+			<div className="text-xl text-muted">
+				No videos found
+			</div>
+		</main>
+	);
 	return (
 		<main className="grid flex-1 h-full sm:h-[100vh] gap-4 overflow-auto grid-col-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 bg-secondary text-white px-4 pb-[8rem]">
 			{data.map((element) => (
 				<VideoItem key={element._id} {...element} />
 			))}
-			
 		</main>
 	);
 };
