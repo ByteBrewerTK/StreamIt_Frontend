d67fe62c3a210cd13cee7d3504d4740379babccb
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-22 10:08:54 +0530

    update - add nav to profile page

---
+++ src/components/user/ProfileNav.jsx
@@ -0,0 +1,28 @@
+import { useNavigate, Link } from "react-router-dom";
+import { MdArrowBack } from "react-icons/md";
+import { GoGear } from "react-icons/go";
+
+const ProfileNav = () => {
+	const navigate = useNavigate();
+	return (
+		<header className="w-full py-2">
+			<nav className="flex items-center justify-between mx-auto w-container">
+				<button
+					onClick={() => {
+						navigate(-1);
+					}}
+				>
+					<MdArrowBack className="text-2xl"/>
+				</button>
+
+				<div>
+					<Link to={"/"}>
+						<GoGear className="text-xl"/>
+					</Link>
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+export default ProfileNav;
