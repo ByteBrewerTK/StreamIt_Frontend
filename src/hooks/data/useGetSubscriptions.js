93a60a16f9fcd63bfd2ae844bc2d58060fd6ad17
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-23 08:30:26 +0530

    add - custom hook for get all subscriptions

---
+++ src/hooks/data/useGetSubscriptions.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { apiRequest } from "../../services/api";
+
+const useGetSubscriptions = () => {
+	const [subscriptionData, setSubscriptionData] = useState(null);
+	const [subscriptionError, setSubscriptionError] = useState(null);
+	const [subscriptionLoading, setSubscriptionLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const {data} = await apiRequest("/subscriptions");
+				setSubscriptionData(data.subscribedChannels);
+			} catch (error) {
+				setSubscriptionError(error);
+                console.log("Error while fetch all subscriptions : ",error)
+			} finally {
+				setSubscriptionLoading(false);
+			}
+		};
+		fetchData();
+	}, []);
+	return { subscriptionData, subscriptionError, subscriptionLoading };
+};
+
+export default useGetSubscriptions;
