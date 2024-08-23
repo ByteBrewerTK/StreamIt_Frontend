ffec0438de6dd7ce5846e782781363ea0aa57fb4
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-23 08:34:22 +0530

    update - ui

--- src/components/ui/RegistrationSuccessPopup.jsx
+++ src/components/ui/RegistrationSuccessPopup.jsx
@@ -1,8 +1,10 @@
+import CTAButton from "./CTAButton";
+
 const RegistrationSuccessPopup = ({ show, onClose }) => {
 	if (!show) return null;
 
 	return (
-		<div className="fixed inset-0 flex items-center justify-center bg-opacity-75 backdrop-blur-sm">
+		<div className="fixed inset-0 z-10 flex items-center justify-center bg-opacity-75 backdrop-blur-sm">
 			<div className="p-6 bg-white rounded-lg shadow-lg">
 				<h2 className="mb-4 text-2xl font-bold text-center">
 					Registration Successful
@@ -11,12 +13,9 @@ const RegistrationSuccessPopup = ({ show, onClose }) => {
 					Thank you for your registration. Please verify your email to
 					continue.
 				</p>
-				<button
-					className="w-full px-4 py-2 font-bold text-white bg-black rounded-full hover:bg-opacity-100 focus:outline-none focus:shadow-outline bg-opacity-80 "
-					onClick={onClose}
-				>
-					Close
-				</button>
+				<div onClick={onClose}>
+					<CTAButton>Close</CTAButton>
+				</div>
 			</div>
 		</div>
 	);
