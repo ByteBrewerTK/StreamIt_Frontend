6d28d8d1f70eb03cf96f82500e32c5000606d02a
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-24 09:02:01 +0530

    refactor - playlist ui

--- src/components/user/Playlists.jsx
+++ src/components/user/Playlists.jsx
@@ -1,21 +1,22 @@
 import PlaylistItem from "../ui/PlaylistItem";
 
 const Playlists = ({ data }) => {
-    console.log(data)
 	return (
-		<div className="my-4 h-[190px]">
+		<div className="my-4 h-[150px]">
 			<div className="mx-2">
-				<h3 className="text-xl font-[500]">Playlists</h3>
+				<h3 className="text-xl font-[500] mx-2">Playlists</h3>
 			</div>
-			<div className="grid w-full grid-flow-col my-2 overflow-x-auto overflow-y-hidden auto-cols-max scrollbar-hide">
-				{data?.length === 0
-					? "No playlist found"
-					: data.map((playlist) => (
-							<div key={playlist._id}>
-								<PlaylistItem {...playlist} />
-							</div>
-					  ))}
-			</div>
+			{data?.length === 0 ? (
+				<div className="grid w-full h-full place-content-center text-muted">No playlists found</div>
+			) : (
+				<div className="grid w-full grid-flow-col my-2 overflow-x-auto overflow-y-hidden auto-cols-max scrollbar-hide">
+					{data.map((playlist) => (
+						<div key={playlist._id}>
+							<PlaylistItem {...playlist} />
+						</div>
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
