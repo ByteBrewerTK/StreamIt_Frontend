1b76a902d0b41adf5f07511cad70a95e9890a53d
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-24 09:09:47 +0530

    add - new routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import ResendVerificationMail from "./pages/auth/ResendVerificationMail";
 import Loader from "./components/ui/loader/Loader";
 import { Toaster, toast } from "react-hot-toast";
 import { useEffect } from "react";
+import NotFound from "./pages/error/NotFound";
+import Account from "./pages/settings/Account";
 
 // Lazy load the component
 const LoginPage = lazy(() => import("./pages/auth/LoginPage"));
@@ -17,6 +19,10 @@ const VerifyOTP = lazy(() => import("./pages/auth/VerifyOTP"));
 const WatchVideo = lazy(() => import("./pages/WatchVideo"));
 const UserProfilePage = lazy(() => import("./pages/UserProfilePage"));
 const SubscriptionsPage = lazy(() => import("./pages/SubscriptionsPage"));
+const SubscriptionsListPage = lazy(() =>
+	import("./pages/SubscriptionsListPage")
+);
+const SettingsPage = lazy(() => import("./pages/settings/SettingsPage"));
 
 function App() {
 	useEffect(() => {
@@ -40,44 +46,62 @@ function App() {
 					}
 				>
 					<Routes>
-						<Route path="/auth/login" element={<LoginPage />} />
-						<Route
-							path="/auth/sign-up"
-							element={<RegistrationPage />}
-						/>
-						<Route
-							path="/auth/reset-password"
-							element={<PasswordResetPage />}
-						/>
-						<Route
-							path="/auth/verify-otp"
-							element={<VerifyOTP />}
-						/>
-						<Route
-							path="/auth/new-password"
-							element={<CreateNewPassword />}
-						/>
-						<Route
-							path="/confirm/:token"
-							element={<VerificationSuccess />}
-						/>
-						<Route
-							path="/resend/confirm/:email"
-							element={<ResendVerificationMail />}
-						/>
+						<Route path="auth">
+							<Route path="login" element={<LoginPage />} />
+							<Route
+								path="sign-up"
+								element={<RegistrationPage />}
+							/>
+							<Route
+								path="reset-password"
+								element={<PasswordResetPage />}
+							/>
+							<Route path="verify-otp" element={<VerifyOTP />} />
+							<Route
+								path="new-password"
+								element={<CreateNewPassword />}
+							/>
+							<Route
+								path="confirm/:token"
+								element={<VerificationSuccess />}
+							/>
+							<Route
+								path="resend/confirm/:email"
+								element={<ResendVerificationMail />}
+							/>
+							<Route path="*" element={<NotFound />} />
+						</Route>
 
 						<Route element={<PrivateRoutes />}>
 							<Route path="/" element={<FeedsPage />} />
 							<Route path="/watch" element={<WatchVideo />} />
-							<Route
-								path="/user/profile"
-								element={<UserProfilePage />}
-							/>
-							<Route
-								path="/user/subscriptions"
-								element={<SubscriptionsPage />}
-							/>
+							<Route path="user">
+								<Route
+									path="profile"
+									element={<UserProfilePage />}
+								/>
+								<Route
+									path="subscriptions"
+									element={<SubscriptionsPage />}
+								/>
+								<Route
+									path="subscriptions/list"
+									element={<SubscriptionsListPage />}
+								/>
+								<Route path="settings">
+									<Route path="" element={<SettingsPage />} />
+									<Route
+										path="account"
+										element={<Account />}
+									>
+										
+									</Route>
+								</Route>
+								<Route path="*" element={<NotFound />} />
+							</Route>
+							<Route path="*" element={<NotFound />} />
 						</Route>
+						<Route path="*" element={<NotFound />} />
 					</Routes>
 				</Suspense>
 			</div>
