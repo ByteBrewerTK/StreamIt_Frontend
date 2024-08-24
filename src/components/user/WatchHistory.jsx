0e6e3e2b3f918c11bba7785289609b2cebef76a9
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-24 09:07:07 +0530

    update - handling errors

--- src/components/user/WatchHistory.jsx
+++ src/components/user/WatchHistory.jsx
@@ -1,20 +1,23 @@
 import VideoItemWatchHistory from "../ui/VideoItemWatchHistory";
 
-
 const WatchHistory = ({ data }) => {
-	console.log(data);
 	return (
 		<div className="my-4 h-[190px] px-2">
 			<div className="mx-2">
 				<h3 className=" text-xl font-[500]">History</h3>
 			</div>
-			<div className="grid w-full grid-flow-col my-2 overflow-x-auto overflow-y-hidden auto-cols-max scrollbar-hide ">
-				{data?.length === 0
-					? "No watched video found"
-					: data.map((video) => (
-							<VideoItemWatchHistory key={video._id} {...video} />
-					  ))}
-			</div>
+
+			{data?.length === 0 ? (
+				<div className="grid w-full h-full place-content-center text-muted">
+					No playlists found
+				</div>
+			) : (
+				<div className="grid w-full grid-flow-col my-2 overflow-x-auto overflow-y-hidden auto-cols-max scrollbar-hide">
+					{data.map((video) => (
+						<VideoItemWatchHistory key={video._id} {...video} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
