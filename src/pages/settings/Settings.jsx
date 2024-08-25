e406fdc649dc6258629e23563c76d5faf1911e90
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-25 23:07:26 +0530

    add - settings options

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,12 @@
+import SettingsOptions from "../../components/settings/SettingsOptions";
+
+const Settings = () => {
+	
+	return (
+		<div className="flex-1">
+			<SettingsOptions />
+		</div>
+	);
+};
+
+export default Settings;
