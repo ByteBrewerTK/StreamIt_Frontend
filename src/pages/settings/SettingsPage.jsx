4d7853750dec3a0e734636ae17bd8c6c5ab6e514
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-25 23:07:10 +0530

    refactor - setting page

--- src/pages/settings/SettingsPage.jsx
+++ src/pages/settings/SettingsPage.jsx
@@ -1,32 +1,33 @@
-import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import { MdArrowBack } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
-import { useOutletContext } from "react-router-dom";
-import SettingsOptions from "../../components/settings/SettingsOptions";
+import useNavVisible from "../../hooks/useNavVisible";
+import useSetNavTitle from "../../hooks/useSetNavTitle";
 
 const SettingsPage = () => {
-	const { setNavVisible } = useOutletContext();
+	useNavVisible();
 	const navigate = useNavigate();
-	useEffect(() => {
-		setNavVisible(false);
+	const {navTitle} = useSetNavTitle("Settings");
+	
 
-		return () => {
-			setNavVisible(true);
-		};
-	}, []);
+	
+	
+
 	return (
 		<main className="flex flex-col overflow-hidden text-white size-full">
 			<header className="w-full py-2 bg-primary">
 				<nav className="flex items-center mx-auto space-x-4 w-container">
-					<span onClick={()=>{navigate(-1)}}>
+					<span
+						onClick={() => {
+							navigate(-1);
+						}}
+					>
 						<MdArrowBack className="text-xl" />
 					</span>
-					<h2 className="text-lg">Settings</h2>
+					<h2 className="text-lg">{navTitle}</h2>
 				</nav>
 			</header>
-			<div className="flex-1 py-4">
-                <SettingsOptions/>
-            </div>
+			<Outlet />
 		</main>
 	);
 };
