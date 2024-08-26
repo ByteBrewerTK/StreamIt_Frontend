e719a85fbf650f5b3f3818648407e0acc446fe58
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-26 20:43:02 +0530

    update - ui

--- src/components/ui/CTAButton.jsx
+++ src/components/ui/CTAButton.jsx
@@ -1,7 +1,7 @@
-const CTAButton = ({ children, disabled = false }) => {
+const CTAButton = ({ children, disabled = false , bg_col = "black"}) => {
 	return (
 		<button
-			className="w-full py-2 font-semibold text-white bg-black rounded-full h-[45px]"
+			className={`w-full py-2 font-semibold text-white bg-${bg_col} rounded-full h-[45px]`}
 			disabled={disabled}
 		>
 			{children}
