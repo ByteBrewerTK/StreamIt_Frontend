a598c539d3e49a18660ebbdb41300c3da4a88520
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-27 13:00:27 +0530

    add - general setting page

---
+++ src/pages/settings/GeneralSettingsPage.jsx
@@ -0,0 +1,11 @@
+const GeneralSettingsPage = () => {
+    
+    return (
+        <div className="mx-auto my-2 w-container">
+            This is general settings page
+        </div>
+    )
+    
+}
+
+export default GeneralSettingsPage
