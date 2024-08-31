bd2b578d13f3b8b36b4c858fab3640762e869106
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-31 22:55:12 +0530

    add - more option panel

--- src/pages/FeedsPage.jsx
+++ src/pages/FeedsPage.jsx
@@ -4,20 +4,41 @@ import Loader from "../components/ui/loader/Loader";
 import { useOutletContext } from "react-router-dom";
 import useContainerScroll from "../hooks/useContainerScroll";
 import useFeedVideos from "../hooks/data/useFeedVideos";
+import { GoBookmark } from "react-icons/go";
+import { PiShareFat } from "react-icons/pi";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+import VideoItemMorePanel from "../components/user/VideoItemMorePanel";
 
 const FeedsPage = () => {
 	const feedRef = useRef(null);
+	const moreOptionsRef = useRef(null);
 	const navbarVisible = useContainerScroll(feedRef);
 	const { setNavVisible } = useOutletContext();
+	const [isMoreOptionsOpen, setMoreOptionsOpen] = useState(false);
+	const [videoId, setVideoId] = useState(null);
 
-	console.log(navbarVisible);
 	useEffect(() => {
-		console.log(navbarVisible);
-		setNavVisible(navbarVisible);
+		document.addEventListener("mousedown", outsideClick);
 
-		return () => setNavVisible(false);
-	}, [navbarVisible, setNavVisible]);
+		return () => {
+			document.removeEventListener("mousedown", outsideClick);
+		};
+	}, []);
 
+	const outsideClick = (event) => {
+		console.log(moreOptionsRef.current.contains(event.target));
+		if (
+			moreOptionsRef.current &&
+			!moreOptionsRef.current.contains(event.target)
+		) {
+			setMoreOptionsOpen(false);
+		}
+	};
+	const moreOptionsHandler = (state, videoId) => {
+		setVideoId(videoId);
+		setMoreOptionsOpen(state);
+	};
 	const query = {
 		limit: 10,
 		page: 1,
@@ -27,7 +48,6 @@ const FeedsPage = () => {
 	const url = `/video?limit=${query.limit}&page=${query.page}&sortBy=${query.sortBy}&sortType=${query.sortType}`;
 
 	const { feedData, feedError, feedLoading } = useFeedVideos(url);
-	
 
 	if (feedLoading)
 		return (
@@ -46,14 +66,47 @@ const FeedsPage = () => {
 			</main>
 		);
 	return (
-		<main
-			ref={feedRef}
-			className="grid flex-1 h-full sm:h-[100vh] gap-4 overflow-x-hidden overflow-y-auto grid-col-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 bg-secondary text-white px-4 scrollbar-hide"
-		>
-			{feedData && feedData?.map((element) => (
-				<VideoItem key={element._id} {...element} />
-			))}
-		</main>
+		<>
+			<main
+				ref={feedRef}
+				className="grid flex-1 h-full sm:h-[100vh] gap-4 overflow-x-hidden overflow-y-auto grid-col-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 bg-secondary text-white scrollbar-hide"
+			>
+				{feedData &&
+					feedData?.map((element) => (
+						<VideoItem
+							key={element._id}
+							element={element}
+							moreOptionsHandler={moreOptionsHandler}
+						/>
+					))}
+			</main>
+			<section
+				className={`absolute flex w-full h-full overflow-hidden transition duration-200 ${
+					!isMoreOptionsOpen ? "translate-y-full" : ""
+				} `}
+			>
+				<section
+					ref={moreOptionsRef}
+					className="bg-secondary z-[100] flex flex-col self-end rounded-t-lg  w-container mx-auto pt-8 relative overflow-hidden "
+				>
+					<span
+						onClick={() => {
+							moreOptionsHandler(false);
+						}}
+						className="absolute top-0 right-0 p-1 text-black bg-white rounded-es-lg "
+					>
+						<IoMdClose />
+					</span>
+					<VideoItemMorePanel
+						isMoreOptionsOpen={isMoreOptionsOpen}
+						setMoreOptionsOpen={setMoreOptionsOpen}
+						videoId={videoId}
+					/>
+				</section>
+			</section>
+
+			<section></section>
+		</>
 	);
 };
 
