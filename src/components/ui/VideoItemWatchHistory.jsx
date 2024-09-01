135ce8369c476a736cd27895117f1f2a4fcac288
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-01 23:04:18 +0530

    refactor - ui and handler

--- src/components/ui/VideoItemWatchHistory.jsx
+++ src/components/ui/VideoItemWatchHistory.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import formatVideoDuration from "../../utils/formatVideoDuration";
 import { IoMdMore } from "react-icons/io";
 
-const VideoItemWatchHistory = ({ _id, thumbnail, owner, title, duration }) => {
+const VideoItemWatchHistory = ({ video, moreOptionsHandler }) => {
+	const { _id, thumbnail, owner, title, duration } = video;
 	return (
-		<Link
-			to={`/watch?v=${_id}`}
-			className="flex flex-col w-[130px] mx-2 h-[145px] shadow-lg my-1"
-		>
-			<div className="relative w-full overflow-hidden rounded-lg aspect-video">
+		<div className="flex flex-col w-[130px] mx-2 h-[145px] shadow-lg my-1">
+			<Link
+				to={`/watch?v=${_id}`}
+				className="relative w-full overflow-hidden rounded-lg aspect-video"
+			>
 				{/* Thumbnail */}
 				<img
 					src={thumbnail}
@@ -19,21 +20,30 @@ const VideoItemWatchHistory = ({ _id, thumbnail, owner, title, duration }) => {
 				<span className="absolute bottom-0 right-0 px-1 mb-1 mr-1 bg-black rounded bg-opacity-55 text-smr">
 					{formatVideoDuration(duration)}
 				</span>
-			</div>
+			</Link>
 			<div className="flex justify-between flex-1 py-2 ">
 				{/* details */}
 				<div className="flex flex-col justify-between">
-					<p className="text-sm leading-4">{title.slice(0, 26)}...</p>
-					<span className="text-[0.6rem] text-muted">
+					<Link to={`/watch?v=${_id}`} className="text-sm leading-4">
+						{title.slice(0, 26)}...
+					</Link>
+					<Link
+						to={`/user/${owner.username}`}
+						className="text-[0.6rem] text-muted"
+					>
 						{owner.fullName}
-					</span>
+					</Link>
 				</div>
 
-				<span>
+				<button
+					onClick={() => {
+						moreOptionsHandler(true, _id);
+					}}
+				>
 					<IoMdMore className="text-[1rem]" />
-				</span>
+				</button>
 			</div>
-		</Link>
+		</div>
 	);
 };
 
