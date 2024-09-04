b4d4431ea6588624fcce5ff259e71da0348adadb
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-04 23:54:00 +0530

    refactor - props de structuring

--- src/components/user/Playlists.jsx
+++ src/components/user/Playlists.jsx
@@ -1,18 +1,26 @@
 import PlaylistItem from "../ui/PlaylistItem";
 
-const Playlists = ({ data }) => {
+const Playlists = ({ data, playlistMoreOptionsHandler }) => {
+	
 	return (
 		<div className="my-4 h-[150px]">
 			<div className="mx-2">
 				<h3 className="text-xl font-[500] mx-2">Playlists</h3>
 			</div>
 			{data?.length === 0 ? (
-				<div className="grid w-full h-full place-content-center text-muted">No playlists found</div>
+				<div className="grid w-full h-full place-content-center text-muted">
+					No playlists found
+				</div>
 			) : (
 				<div className="grid w-full grid-flow-col my-2 overflow-x-auto overflow-y-hidden auto-cols-max scrollbar-hide">
 					{data.map((playlist) => (
 						<div key={playlist._id}>
-							<PlaylistItem {...playlist} />
+							<PlaylistItem
+								playlist={playlist}
+								playlistMoreOptionsHandler={
+									playlistMoreOptionsHandler
+								}
+							/>
 						</div>
 					))}
 				</div>
