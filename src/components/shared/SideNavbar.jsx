12067cbbfa1c667689d7de0eb06e0581199eefdd
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-05 21:08:33 +0530

    update - side bar on state change

--- src/components/shared/SideNavbar.jsx
+++ src/components/shared/SideNavbar.jsx
@@ -1,16 +1,16 @@
 import { sideNavItems } from "../../data/navItems";
 import SideNavItem from "../SideNavItem";
 
-const SideNavbar = () => {
+const SideNavbar = ({isSidebarOpen}) => {
 	return (
-		<aside className="hidden w-[15rem] bg-primary h-full text-white px-4 ">
+		<aside
+			className={`w-[15rem] bg-primary h-full text-white px-4 transition-all overflow-hidden text-nowrap ${isSidebarOpen ? "w-[15rem] translate-x-0" : "w-0  px-0 "} `}
+		>
 			<nav className="flex flex-col space-y-4">
 				{sideNavItems.map((element, index) => (
 					<SideNavItem key={index} {...element} />
-					// <div>hello</div>
 				))}
 			</nav>
-			
 		</aside>
 	);
 };
