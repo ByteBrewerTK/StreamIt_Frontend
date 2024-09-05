474f17bd6fb88b882eb642f0923d63758c0ece68
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-05 20:34:14 +0530

    update - navbar handling

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,15 +1,21 @@
 import { FaRegBell } from "react-icons/fa6";
 import { RiBardLine } from "react-icons/ri";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { RxHamburgerMenu } from "react-icons/rx";
 import logo from "../../assets/brand/StreamItLogo_Horizon_.png";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+const Navbar = ({ sidebarHandler }) => {
 	return (
 		<header className="flex items-center w-full bg-primary h-navbar">
 			<div className="flex items-center justify-between mx-auto w-container md:w-[95%] z-10">
-				<Link to={"/"}>
-					<img src={logo} alt="" width={120} height={30} />
-				</Link>
+				<div className="flex items-center gap-x-2">
+					<button onClick={() => sidebarHandler()}>
+						<RxHamburgerMenu className="text-2xl text-white" />
+					</button>
+					<Link to={"/"}>
+						<img src={logo} alt="" width={120} height={30} />
+					</Link>
+				</div>
 
 				<div className="flex space-x-4">
 					<span>
