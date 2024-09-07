7db9aae63a8fd932e5e91809f9dad3dc7e9b4700
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-07 22:17:33 +0530

    update - make dynamic content

--- src/components/shared/DeviceAccessDenied.jsx
+++ src/components/shared/DeviceAccessDenied.jsx
@@ -1,11 +1,15 @@
-const DeviceAccessDenied = () => {
+const DeviceAccessDenied = ({type = "Mobile"}) => {
 	return (
-		<div className="z-10 p-8 bg-red-100 rounded shadow-lg">
-			<h1 className="text-xl font-bold text-red-600">Access Denied</h1>
-			<p className="text-red-800">
-				This page can only be accessed on a Mobile device.
-			</p>
-		</div>
+		<main className="flex-1 p-4">
+			<div className="z-10 w-full p-8 bg-red-100 rounded shadow-lg">
+				<h1 className="text-xl font-bold text-red-600">
+					Access Denied
+				</h1>
+				<p className="text-red-800">
+					This page can only be accessed on a {type} device.
+				</p>
+			</div>
+		</main>
 	);
 };
 
