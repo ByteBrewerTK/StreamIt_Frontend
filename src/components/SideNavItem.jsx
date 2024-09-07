b5ba9c8e9d9eae9e84a28e76b15ec57a679503c3
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-07 22:16:28 +0530

    refactor - nav items code

--- src/components/SideNavItem.jsx
+++ src/components/SideNavItem.jsx
@@ -1,14 +1,21 @@
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const SideNavItem = (element) => {
+	const { pathname } = useLocation();
 	const Icon = element.icon;
+	const ActiveIcon = element.activeIcon;
 	return (
 		<Link
 			to={element.path}
-			className="flex items-center px-2 py-2 space-x-2 rounded-lg hover:bg-secondary"
+			className={`flex items-center px-2 py-2 space-x-2 rounded-lg hover:bg-secondary ${element.path === pathname ? "bg-secondary" : ""}`}
 		>
 			<div>
-				<Icon className="text-[20px] text-white" />
+				{element.path === pathname ? (
+					<ActiveIcon className="text-[20px] text-white" />
+				) : (
+					<Icon className="text-[20px] text-white" />
+				)}
 			</div>
 			<span>{element.name}</span>
 		</Link>
