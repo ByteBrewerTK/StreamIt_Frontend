e8ddac444564e233293b5f6b3d5ea2540dda598a
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-07 22:20:55 +0530

    update - nav item active icon

--- src/data/navItems.js
+++ src/data/navItems.js
@@ -5,41 +5,54 @@ import { RiPlayList2Fill } from "react-icons/ri";
 import { MdOutlineVideoLibrary } from "react-icons/md";
 import { LuThumbsUp } from "react-icons/lu";
 import { GrHistory } from "react-icons/gr";
+import { GoHomeFill } from "react-icons/go";
+import { MdSubscriptions } from "react-icons/md";
+import { BiSolidTv } from "react-icons/bi";
+import { RiPlayList2Line } from "react-icons/ri";
+import { MdVideoLibrary } from "react-icons/md";
+import { IoMdThumbsUp } from "react-icons/io";
 
 export const sideNavItems = [
 	{
 		name: "Home",
 		icon: GoHome,
+		activeIcon: GoHomeFill,
 		path: "/",
 	},
 	{
 		name: "Subscriptions",
-		icon:  MdOutlineSubscriptions,
+		icon: MdOutlineSubscriptions,
+		activeIcon: MdSubscriptions,
 		path: "/user/subscriptions",
 	},
 	{
 		name: "Your Channel",
-		icon:  GrChannel,
+		icon: GrChannel,
+		activeIcon: BiSolidTv,
 		path: "/user/channel",
 	},
 	{
 		name: "History",
-		icon:  GrHistory,
+		icon: GrHistory,
+		activeIcon: GrHistory,
 		path: "/user/history",
 	},
 	{
 		name: "Playlist",
-		icon:  RiPlayList2Fill,
+		icon: RiPlayList2Line,
+		activeIcon: RiPlayList2Fill,
 		path: "/user/playlists",
 	},
 	{
 		name: "Your Videos",
-		icon:  MdOutlineVideoLibrary,
+		icon: MdOutlineVideoLibrary,
+		activeIcon: MdVideoLibrary,
 		path: "/user/videos",
 	},
 	{
 		name: "Liked Videos",
-		icon:  LuThumbsUp,
+		icon: LuThumbsUp,
+		activeIcon: IoMdThumbsUp,
 		path: "/user/liked-videos",
 	},
 ];
