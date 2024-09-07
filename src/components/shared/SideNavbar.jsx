dd1c8644a1e1ad3eca568b75cf3eda413192571e
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-07 22:20:32 +0530

    update - conditional toggle

--- src/components/shared/SideNavbar.jsx
+++ src/components/shared/SideNavbar.jsx
@@ -1,11 +1,13 @@
 import { sideNavItems } from "../../data/navItems";
 import SideNavItem from "../SideNavItem";
 
-const SideNavbar = ({isSidebarOpen}) => {
+const SideNavbar = ({ isSidebarOpen }) => {
 	return (
 		<aside
 			className={`bg-primary h-full text-white  transition-all overflow-hidden text-nowrap hidden md:block ${
-				isSidebarOpen ? "w-[15rem] px-4" : " -translate-x-full w-0  px-0 "
+				isSidebarOpen
+					? "w-[15rem] px-4"
+					: " -translate-x-full w-0  px-0 "
 			} `}
 		>
 			<nav className="flex flex-col space-y-4">
