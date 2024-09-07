73b5c8a63543f18e3e2e72683c46acbbb71c982a
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-07 22:17:12 +0530

    update - conditional ui change

--- src/components/settings/OptionsItem.jsx
+++ src/components/settings/OptionsItem.jsx
@@ -1,12 +1,19 @@
 ("react-icons/md");
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const OptionsItem = ({ title, icon, path }) => {
+	const { pathname } = useLocation();
+	const currentPath = pathname.split("/").at(-1);
+	console.log(path, currentPath);
+
 	const Icon = icon;
 	return (
 		<Link
 			to={path}
-			className="flex items-center px-4 py-2 space-x-2 hover:bg-white hover:bg-opacity-10 group"
+			className={`flex items-center px-4 py-2 space-x-2 hover:bg-white hover:bg-opacity-10 group ${
+				currentPath === path ? " bg-white bg-opacity-10" : ""
+			}`}
 		>
 			<Icon className="text-xl" />
 			<span>{title}</span>
