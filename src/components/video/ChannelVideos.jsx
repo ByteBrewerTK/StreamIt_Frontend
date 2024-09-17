8144d7d95ba83cc0181221704caa8b83a9895515
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-17 23:16:35 +0530

    update - ui

--- src/components/video/ChannelVideos.jsx
+++ src/components/video/ChannelVideos.jsx
@@ -32,7 +32,7 @@ const ChannelVideos = (username) => {
 	}
 
 	return (
-		<div className="py-2 mx-auto w-container">
+		<div className="grid flex-1 h-full sm:h-[100vh] gap-x-4 overflow-x-hidden overflow-y-auto grid-col-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 bg-secondary text-white scrollbar-hide ">
 			{channelVideosData.map((video) => (
 				<VideoItem key={video._id} element={video} />
 			))}
