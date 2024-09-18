368f7678ecc4e953a1932a170bc3098b43a37803
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-18 23:07:48 +0530

    add - history item

---
+++ src/components/video/HistoryVideoItem.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import formatVideoDuration from "../../utils/formatVideoDuration"
+
+const HistoryVideoItem = ({ item }) => {
+	console.log(item);
+	return (
+		<div>
+			<Link to={`watch?v=${item._id}`}>
+				<img src={item.thumbnail} alt="" loading="lazy" />
+        <span>{formatVideoDuration(item.duration)}</span>
+			</Link>
+			<div>
+        <h3>{item.title}</h3>
+        <div>
+          <Link>{item.owner.fullName}</Link>
+          <span>{item.views}</span>
+        </div>
+      </div>
+		</div>
+	);
+};
+
+export default HistoryVideoItem;
