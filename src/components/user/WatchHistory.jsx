2e483c7331608375b307bc77c3cdf4549aaad590
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-19 23:47:59 +0530

    add - history view all button

--- src/components/user/WatchHistory.jsx
+++ src/components/user/WatchHistory.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
 import VideoItemWatchHistory from "../ui/VideoItemWatchHistory";
 
 const WatchHistory = ({ data, moreOptionsHandler }) => {
 	return (
 		<div className="my-4 h-[190px] px-2">
-			<div className="mx-2">
+			<div className="flex items-center justify-between mx-2">
 				<h3 className=" text-xl font-[500]">History</h3>
+				<Link to={"/user/history"} className="text-blue-500 text-smr">View all</Link>
 			</div>
 
 			{data?.length === 0 ? (
@@ -18,7 +20,6 @@ const WatchHistory = ({ data, moreOptionsHandler }) => {
 							key={video._id}
 							video={video}
 							moreOptionsHandler={moreOptionsHandler}
-							
 						/>
 					))}
 				</div>
