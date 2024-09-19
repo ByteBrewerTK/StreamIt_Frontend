720ed32a014ea327f5954fcb19d334778f1250fe
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-19 23:55:42 +0530

    update - add view all playlist button

--- src/components/user/Playlists.jsx
+++ src/components/user/Playlists.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
 import PlaylistItem from "../ui/PlaylistItem";
 
 const Playlists = ({ data, playlistMoreOptionsHandler }) => {
 	
 	return (
 		<div className="my-4 h-[150px]">
-			<div className="mx-2">
-				<h3 className="text-xl font-[500] mx-2">Playlists</h3>
+			<div className="flex items-center justify-between mx-2">
+				<h3 className=" text-xl font-[500]">History</h3>
+				<Link to={"/user/playlists"} className="text-blue-500 text-smr">
+					View all
+				</Link>
 			</div>
 			{data?.length === 0 ? (
 				<div className="grid w-full h-full place-content-center text-muted">
