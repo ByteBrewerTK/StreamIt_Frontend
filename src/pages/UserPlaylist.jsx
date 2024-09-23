d4e957abd5ae01badaf13117945f3c26025853c0
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-23 23:00:26 +0530

    update - playlist page

--- src/pages/UserPlaylist.jsx
+++ src/pages/UserPlaylist.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import usePlaylist from "../hooks/data/usePlaylist";
+import { getUserData } from "../services/authServices";
+import Playlists from "../components/user/Playlists";
+import PlaylistMorePanel from "../components/playlist/PlaylistMorePanel";
+import Loader from "../components/ui/loader/Loader";
+
 const UserPlaylist = () => {
-	return <div>This is playlist</div>;
+	const localUser = getUserData();
+	const { playlistData, playlistLoading, playlistError } = usePlaylist(
+		localUser._id
+	);
+	const [playlistId, setPlaylistId] = useState(null);
+	const [isPlaylistMoreOptionsOpen, setPlaylistMoreOptionsOpen] =
+		useState(false);
+	const playlistMoreOptionsHandler = (state, playlistId) => {
+		setPlaylistId(playlistId);
+		setPlaylistMoreOptionsOpen(state);
+	};
+	if (playlistLoading) {
+		return (
+			<div className="grid w-full h-full place-items-center">
+				<span className="size-[4rem]">
+					<Loader />
+				</span>
+			</div>
+		);
+	}
+
+	if (playlistError) {
+		return <div>Error loading user data.</div>;
+	}
+
+	if (!playlistData) {
+		return <div>No Playlist found</div>;
+	}
+	return (
+		<>
+			<Playlists
+				data={playlistData.data}
+				playlistMoreOptionsHandler={playlistMoreOptionsHandler}
+			/>
+			<PlaylistMorePanel
+				isPlaylistMoreOptionsOpen={isPlaylistMoreOptionsOpen}
+				setPlaylistMoreOptionsOpen={setPlaylistMoreOptionsOpen}
+				playlistId={playlistId}
+			/>
+		</>
+	);
 };
 
 export default UserPlaylist;
