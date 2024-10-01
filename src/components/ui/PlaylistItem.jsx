ffb218a41c071a711d4ca41f19d2049554321878
Abutalif Khan <talifkhan1@hotmail.com>
2024-10-01 22:59:18 +0530

    update - ui

--- src/components/ui/PlaylistItem.jsx
+++ src/components/ui/PlaylistItem.jsx
@@ -5,18 +5,20 @@ const PlaylistItem = ({ playlist, playlistMoreOptionsHandler }) => {
 	const { _id, name, videos } = playlist;
 	const banner = videos[0].thumbnail;
 	return (
-		<div className="relative flex flex-col w-[130px] mx-2 h-[145px] shadow-lg my-1 text-white before:w-[90%] before:h-[10%] before:bg-gray-500 before:absolute before:left-[50%] before:-translate-x-[50%] before:-top-1 before:rounded-full">
+		<div className="relative md:flex md:flex-col md:w-[130px] mx-2 md:h-[145px] shadow-lg my-1 text-white grid grid-cols-2 w-full md:before:w-[90%] md:before:h-[10%] md:before:bg-gray-500 md:before:absolute md:before:left-[50%] md:before:-translate-x-[50%] md:before:-top-1 md:before:rounded-full">
 			<Link
 				to={`/playlist/${_id}`}
-				className="relative w-full overflow-hidden rounded-lg aspect-video "
+				className="relative w-full overflow-hidden border rounded-lg aspect-video"
 			>
 				{/* Banner */}
-				<img
-					src={banner}
-					alt=""
-					loading="lazy"
-					className="object-cover object-center "
-				/>
+				<div className="w-full">
+					<img
+						src={banner}
+						alt=""
+						loading="lazy"
+						className="object-cover object-center "
+					/>
+				</div>
 				<span className="absolute bottom-0 right-0 flex items-center gap-1 px-1 mb-1 mr-1 bg-black rounded bg-opacity-55 text-smr">
 					<RiPlayList2Fill />
 					<span>{videos?.length}</span>
