4f6d46c97a238e682c09e63c2226d40ab7899170
Abutalif Khan <talifkhan1@hotmail.com>
2024-10-18 22:31:38 +0530

    update - state and data

--- src/pages/desktop/UserVideosPage.jsx
+++ src/pages/desktop/UserVideosPage.jsx
@@ -1,13 +1,17 @@
 import DeviceAccessDenied from "../../components/shared/DeviceAccessDenied";
+import useGetUserAllVideos from "../../hooks/data/useGetUserAllVideos";
 import useDeviceType from "../../hooks/useDeviceType";
+import { getUserData } from "../../services/authServices";
 
 const UserVideosPage = () => {
+	const { username } = getUserData();
+	const { data, error, loading } = useGetUserAllVideos(username);
 	const deviceType = useDeviceType();
 
 	if (!deviceType === "Desktop") {
 		return <DeviceAccessDenied type="Desktop" />;
 	}
-	return <div>This is user videos section</div>;
+	return <main></main>;
 };
 
 export default UserVideosPage;
