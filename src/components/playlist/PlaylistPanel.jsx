12bffaf2f0c771e50e5c371e5e6f4e61f90ef769
Abutalif Khan <talifkhan1@hotmail.com>
2024-10-23 23:42:00 +0530

    fix - playlist panel ui

--- src/components/playlist/PlaylistPanel.jsx
+++ src/components/playlist/PlaylistPanel.jsx
@@ -53,7 +53,7 @@ const PlaylistPanel = ({ setMoreOptionsOpen, videoId }) => {
 	}
 
 	return (
-		<div className="flex flex-col max-h-[20rem]">
+		<div className="flex flex-col max-h-[20rem] border">
 			{playlistPanelOpen && (
 				<CreatePlaylistModal
 					onClose={() => createPlaylistModalHandler(false)}
