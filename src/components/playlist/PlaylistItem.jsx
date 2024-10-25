6bc9b1ad64c29e4c14164b197fe2ae5576abad40
Abutalif Khan <talifkhan1@hotmail.com>
2024-10-25 17:17:19 +0530

    format - code

--- src/components/playlist/PlaylistItem.jsx
+++ src/components/playlist/PlaylistItem.jsx
@@ -1,14 +1,22 @@
-import { IoMdClose, IoMdMore } from "react-icons/io";
+import { IoMdMore } from "react-icons/io";
 import { Link } from "react-router-dom";
 import formatVideoDuration from "../../utils/formatVideoDuration";
 import { formatCounts } from "../../utils/formatCounts";
 import { getTimeAgo } from "../../utils/getTimeAgo";
 
-const PlaylistItem = ({ _id, thumbnail, title, owner, views, duration, createdAt }) => {
+const PlaylistItem = ({
+	_id,
+	thumbnail,
+	title,
+	owner,
+	views,
+	duration,
+	createdAt,
+}) => {
 	return (
 		<div className="grid w-full grid-cols-2 lg:flex lg:flex-row gap-x-4">
 			<Link to={`/watch?v=${_id}`}>
-				<div className="relative overflow-hidden rounded-lg lg:max-w-[300px] aspect-video">
+				<div className="relative overflow-hidden rounded-lg lg:max-w-[200px] aspect-video">
 					<img
 						src={thumbnail}
 						alt=""
@@ -20,26 +28,23 @@ const PlaylistItem = ({ _id, thumbnail, title, owner, views, duration, createdAt
 					</span>
 				</div>
 			</Link>
-			<div className="flex-1">
-				<div className="flex justify-between w-full lg:text-2xl">
+			<div className="flex flex-1">
+				<div className="flex w-full lg:text-2xl md:flex-col gap-y-2">
 					<h3 className=" text-wrap text-smr md:text-xl">{title}</h3>
-					<div>
-						<button className="hidden md:inline">
-							<IoMdClose />
-						</button>
-						<button>
-							<IoMdMore />
-						</button>
+
+					<div className="flex gap-x-2 text-muted_dark text-[.76rem] flex-col md:flex-row leading-none">
+						<Link to={`/user/@${owner.username}`}>
+							{owner.fullName}
+						</Link>
+						<span className="hidden md:inline">&bull;</span>
+						<span>{formatCounts(views)}</span>
+						<span className="hidden md:inline">&bull;</span>
+						<span>{getTimeAgo(createdAt)}</span>
 					</div>
 				</div>
-				<div className="flex gap-x-2 text-muted_dark text-[.76rem] flex-col md:flex-row leading-none">
-					<Link to={`/user/@${owner.username}`}>
-						{owner.fullName}
-					</Link>
-					<span className="hidden md:inline">&bull;</span>
-					<span>{formatCounts(views)}</span>
-					<span>{getTimeAgo(createdAt)}</span>
-				</div>
+				<button className="h-full text-xl ">
+					<IoMdMore />
+				</button>
 			</div>
 		</div>
 	);
