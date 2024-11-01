d9f1a210bc5572951c2fe3e4a8577b82c7af5195
Abutalif Khan <talifkhan1@hotmail.com>
2024-11-01 22:48:51 +0530

    update - ui

--- src/components/OauthUi.jsx
+++ src/components/OauthUi.jsx
@@ -3,16 +3,18 @@ import { FcGoogle } from "react-icons/fc";
 
 const OauthUi = () => {
 	return (
-		<div className="relative space-y-2 -z-10">
+		<div className="relative z-10 space-y-2">
 			<div className="flex items-center justify-between py-2 space-x-4 select-none">
 				<span className="w-full h-px bg-muted_border" />
 				<span className="font-semibold text-muted text-smr">OR</span>
 				<span className="w-full h-px bg-muted_border" />
 			</div>
-			<div className="flex justify-center py-2 space-x-2 border rounded-full border-muted_border">
+			<button
+				className="flex justify-center w-full py-2 space-x-2 border rounded-full cursor-pointer border-muted_border"
+			>
 				<FcGoogle className="text-mid" />
 				<span className="text-smr">Sign in with Google</span>
-			</div>
+			</button>
 			<div className="flex justify-center py-2 space-x-2 border rounded-full border-muted_border">
 				<FaGithub className="text-mid" />
 				<span className="text-smr">Sign in with GitHub</span>
