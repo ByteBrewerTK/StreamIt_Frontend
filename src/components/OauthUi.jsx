d761d03e7e4f634118b4c60721cb3e8a0ea22a9d
Abutalif Khan <talifkhan1@hotmail.com>
2024-11-04 17:51:02 +0530

    update - google auth handler

--- src/components/OauthUi.jsx
+++ src/components/OauthUi.jsx
@@ -1,7 +1,12 @@
-import { FaGithub } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
+// import { FaGithub } from "react-icons/fa6";
 
 const OauthUi = () => {
+	const googleAuthHandler = () => {
+		window.location.href = `${
+			import.meta.env.VITE_API_URL
+		}/user/auth/google`;
+	};
 	return (
 		<div className="relative z-10 space-y-2">
 			<div className="flex items-center justify-between py-2 space-x-4 select-none">
@@ -10,15 +15,16 @@ const OauthUi = () => {
 				<span className="w-full h-px bg-muted_border" />
 			</div>
 			<button
-				className="flex justify-center w-full py-2 space-x-2 border rounded-full cursor-pointer border-muted_border"
+				onClick={googleAuthHandler}
+				className="relative flex justify-center w-full py-2 space-x-2 border rounded-full cursor-pointer border-muted_border"
 			>
 				<FcGoogle className="text-mid" />
 				<span className="text-smr">Sign in with Google</span>
 			</button>
-			<div className="flex justify-center py-2 space-x-2 border rounded-full border-muted_border">
+			{/* <div className="flex justify-center py-2 space-x-2 border rounded-full border-muted_border">
 				<FaGithub className="text-mid" />
 				<span className="text-smr">Sign in with GitHub</span>
-			</div>
+			</div> */}
 		</div>
 	);
 };
