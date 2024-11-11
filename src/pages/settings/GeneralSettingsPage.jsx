2f0deb8af9baa6ef9e36b2f3e26fdcad4f3ca5cf
Abutalif Khan <talifkhan1@hotmail.com>
2024-11-11 23:54:42 +0530

    update - page load logic, settings options, import get all settings hooks

--- src/pages/settings/GeneralSettingsPage.jsx
+++ src/pages/settings/GeneralSettingsPage.jsx
@@ -3,23 +3,43 @@ import { AiOutlinePlayCircle } from "react-icons/ai";
 import { IoPlayForwardOutline } from "react-icons/io5";
 import useDeviceType from "../../hooks/useDeviceType";
 import DeviceAccessDenied from "../../components/shared/DeviceAccessDenied";
+import useGetSettings from "../../hooks/data/useGetSettings";
+import Loader from "../../components/ui/loader/Loader";
 const GeneralSettingsPage = () => {
 	const deviceType = useDeviceType();
+	const { settingsData, settingsError, settingsLoading } = useGetSettings();
+
+	if (deviceType === "Desktop") {
+		return <DeviceAccessDenied />;
+	}
+	if (settingsLoading || settingsError) {
+		return (
+			<section className="grid size-full place-items-center">
+				{settingsError ? (
+					<span>{settingsError}</span>
+				) : (
+					<span className="size-[70px]">
+						<Loader />
+					</span>
+				)}
+			</section>
+		);
+	}
+	const { autoPlayOnStart, autoPlayNext } = settingsData;
 	const options = [
 		{
 			title: "Auto Play on Start",
 			icon: AiOutlinePlayCircle,
-			path: "password",
+			opt: "autoPlayOnStart",
+			isEnabled: autoPlayOnStart,
 		},
 		{
 			title: "Auto Play Next",
 			icon: IoPlayForwardOutline,
-			path: "password",
+			opt: "autoPlayStart",
+			isEnabled: autoPlayNext,
 		},
 	];
-	if (deviceType === "Desktop") {
-		return <DeviceAccessDenied />;
-	}
 	return (
 		<section className="w-full mx-auto">
 			{options.map((option, index) => (
