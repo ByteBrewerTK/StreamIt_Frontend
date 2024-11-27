f9ebd14b2789dcb682aa5b63f6079ab052ebb9c8
Abutalif Khan <talifkhan1@hotmail.com>
2024-11-27 23:22:25 +0530

    add - search item

---
+++ src/components/chat/SearchItem.jsx
@@ -0,0 +1,16 @@
+const SearchItem = ({ avatar, fullName, username, _id }) => {
+	if (!avatar || !fullName || !username) return null;
+	return (
+		<div className="flex w-full p-2 text-white border-b border-muted gap-x-2">
+			<div className="size-[40px] rounded-full overflow-hidden border">
+				<img src={avatar} alt="" />
+			</div>
+			<div className="flex-1 w-full">
+				<h3>{fullName}</h3>
+				<p className="truncate text-smr text-muted">@{username}</p>
+			</div>
+		</div>
+	);
+};
+
+export default SearchItem;
