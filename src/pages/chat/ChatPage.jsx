801a5ae5ddd81e25d3f8fa7e3e38e75e9639746f
Abutalif Khan <talifkhan1@hotmail.com>
2024-11-28 23:34:18 +0530

    update - add chat header and all chats section

--- src/pages/chat/ChatPage.jsx
+++ src/pages/chat/ChatPage.jsx
@@ -1,17 +1,39 @@
 import { useEffect } from "react";
-import ChatHeader from "../../components/chat/ChatHeader";
+import ChatPageHeader from "../../components/chat/ChatPageHeader";
 import { useOutletContext } from "react-router-dom";
+import useFetchAllChats from "../../hooks/data/useFetchAllChats";
+import Loader from "../../components/ui/loader/Loader";
+import ChatItem from "../../components/chat/ChatItem";
 const ChatPage = () => {
 	const { setNavVisible } = useOutletContext();
+	const { chatData, chatDataError, chatDataLoading } = useFetchAllChats();
 
 	useEffect(() => {
 		setNavVisible(false);
 		return () => setNavVisible(true);
 	}, [setNavVisible]);
+
 	return (
-		<div>
-			<ChatHeader />
-		</div>
+		<main className="flex flex-col flex-1 border">
+			<ChatPageHeader />
+			<section className="flex-1 border border-red-500">
+				{chatDataLoading ? (
+					<div className="grid size-full place-items-center">
+						<span className="size-[70px]">
+							<Loader />
+						</span>
+					</div>
+				) : chatDataError ? (
+					<div className="grid size-full place-items-center">
+						<p>{chatDataError}</p>
+					</div>
+				) : (
+					chatData.map((chat) => (
+						<ChatItem {...chat} key={chat._id} />
+					))
+				)}
+			</section>
+		</main>
 	);
 };
 
