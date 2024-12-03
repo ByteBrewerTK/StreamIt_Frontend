9063ced24d93278199ee7b3a54711cced3d10fef
Abutalif Khan <talifkhan1@hotmail.com>
2024-12-03 20:07:56 +0530

    update - search items validation

--- src/components/chat/SearchItem.jsx
+++ src/components/chat/SearchItem.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
 const SearchItem = ({ avatar, fullName, username, _id }) => {
-	if (!avatar || !fullName || !username) return null;
+	if (!avatar || !fullName || !username || !_id) return null;
 	return (
 		<Link
-			to={"access"}
+			to={`access/${_id}`}
 			className="flex w-full p-2 text-white border-b border-muted gap-x-2"
 		>
 			<div className="size-[40px] rounded-full overflow-hidden border">
