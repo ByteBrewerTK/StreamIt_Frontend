3f2c697f5caec4574f8e7a0f7248249d49fd41e6
Abutalif Khan <talifkhan1@hotmail.com>
2024-12-03 20:10:11 +0530

    add - chat item comp

--- src/components/chat/ChatItem.jsx
+++ src/components/chat/ChatItem.jsx
@@ -1,29 +1,89 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { ChatState } from "../../contexts/chatContext";
+import { sliceTextAndTruncate } from "../../utils/sliceTextAndTruncate";
 
 // eslint-disable-next-line react/prop-types
-const ChatItem = ({ _id, chatName, isGroupChat, latestMessage, users }) => {
-    if (!chatName || !_id || !chatName || !latestMessage) {
-        return null;
+const ChatItem = (chatItemData) => {
+	const navigate = useNavigate();
+	const { _id, chatName, isGroupChat, latestMessage, users, groupAvatar } =
+		chatItemData;
+	const { user, setSelectedChat } = ChatState();
+	if (!_id || !users.length > 0) {
+		return null;
 	}
-    console.log(latestMessage.sender.username);
+
+	let individualChatWith = null;
+	if (!isGroupChat) {
+		[individualChatWith] = users.filter((singleUser) => {
+			if (singleUser._id !== user._id) return singleUser;
+		});
+	}
+	
+	const clickHandler = () => {
+		setSelectedChat(chatItemData);
+
+		navigate(`messaging/${_id}`);
+	};
 	return (
-		<Link to={`messaging/${_id}`}>
-			<div className="size-[50px] rounded-full overflow-hidden">
-				<img src={users[0].avatar} alt="" />
-			</div>
-			<div>
-				<h3>{isGroupChat ? chatName : users[0].fullName}</h3>
-				{latestMessage ? (
+		<button
+			onClick={clickHandler}
+			className="flex items-center p-2 text-white rounded-lg bg-primary gap-x-2"
+		>
+			{!isGroupChat && individualChatWith ? (
+				<>
+					<div className="size-[50px] rounded-full overflow-hidden">
+						<img src={individualChatWith.avatar} alt="" />
+					</div>
 					<div>
-						{!isGroupChat && (
-							<span>@{latestMessage.sender.username}</span>
+						<h3 className="text-lg font-semibold text-start">
+							{sliceTextAndTruncate(individualChatWith.fullName, 15)}
+						</h3>
+
+						<p className="text-sm text-muted text-start">
+							{latestMessage ? (
+								latestMessage.sender._id === user._id ? (
+									<>
+										<span className="semi-bold">You: </span>
+										{sliceTextAndTruncate(latestMessage.content, 25)}
+									</>
+								) : (
+									sliceTextAndTruncate(latestMessage.content, 25)
+								)
+							) : (
+								"No messages"
+							)}
+						</p>
+					</div>
+				</>
+			) : (
+				<>
+					<div className="size-[50px] rounded-full overflow-hidden">
+						<img src={groupAvatar} alt="" />
+					</div>
+					<div>
+						<h3 className="text-lg font-semibold">
+							{sliceTextAndTruncate(chatName, 20)}
+						</h3>
+						{latestMessage ? (
+							<div className="flex text-sm gap-x-1 text-muted">
+								{latestMessage.sender._id === user._id ? (
+									<span className="font-semibold">
+										You :{" "}
+									</span>
+								) : (
+									<span>
+										@{latestMessage.sender.username}
+									</span>
+								)}
+								<p>{latestMessage.content}</p>
+							</div>
+						) : (
+							<p>No messages</p>
 						)}
 					</div>
-				) : (
-					<p> No messages</p>
-				)}
-			</div>
-		</Link>
+				</>
+			)}
+		</button>
 	);
 };
 
