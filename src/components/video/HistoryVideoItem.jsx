67ea7bc29ec72a726133b42a4303527a3b71e661
Abutalif Khan <talifkhan1@hotmail.com>
2024-12-04 23:56:29 +0530

    fix - username undefined by validation values

--- src/components/video/HistoryVideoItem.jsx
+++ src/components/video/HistoryVideoItem.jsx
@@ -9,13 +9,25 @@ import { apiRequest } from "../../services/api";
 
 const HistoryVideoItem = ({ item }) => {
 	const [loading, setLoading] = useState(false);
+	const { _id, thumbnail, duration, title, owner, views } = item;
+
+	if (
+		!item ||
+		!_id ||
+		!thumbnail ||
+		!duration ||
+		!title ||
+		!owner ||
+		!(views >= 0)
+	)
+		return null;
 
 	const removeFromHistoryHandler = async () => {
 		if (loading) return;
 		setLoading(true);
 		try {
 			await apiRequest(
-				`/user/watch-history/remove/${item._id}`,
+				`/user/watch-history/remove/${_id}`,
 				"DELETE"
 			);
 			toast.success("Removed from watch history");
@@ -28,22 +40,24 @@ const HistoryVideoItem = ({ item }) => {
 	};
 	return (
 		<div className="grid w-full grid-cols-2 lg:flex lg:flex-row gap-x-4">
-			<Link to={`/watch?v=${item._id}`}>
+			<Link to={`/watch?v=${_id}`}>
 				<div className="relative overflow-hidden rounded-lg lg:max-w-[300px] aspect-video">
 					<img
-						src={item.thumbnail}
+						src={thumbnail}
 						alt=""
 						loading="lazy"
 						className="object-cover object-center"
 					/>
 					<span className="absolute px-1 bg-black bg-opacity-50 rounded-lg bottom-2 right-2">
-						{formatVideoDuration(item.duration)}
+						{formatVideoDuration(duration)}
 					</span>
 				</div>
 			</Link>
 			<div className="flex-1">
 				<div className="flex justify-between w-full lg:text-2xl">
-					<h3 className=" text-wrap text-smr md:text-xl">{item.title}</h3>
+					<h3 className=" text-wrap text-smr md:text-xl">
+						{title}
+					</h3>
 					<div>
 						<button
 							onClick={removeFromHistoryHandler}
@@ -57,11 +71,11 @@ const HistoryVideoItem = ({ item }) => {
 					</div>
 				</div>
 				<div className="flex gap-x-2 text-muted_dark text-[.76rem] flex-col md:flex-row leading-none">
-					<Link to={`/user/@${item.owner.username}`}>
-						{item.owner.fullName}
+					<Link to={`/user/@${owner.username}`}>
+						{owner.fullName}
 					</Link>
 					<span className="hidden md:inline">&bull;</span>
-					<span>{formatCounts(item.views)}</span>
+					<span>{formatCounts(views)}</span>
 				</div>
 			</div>
 		</div>
