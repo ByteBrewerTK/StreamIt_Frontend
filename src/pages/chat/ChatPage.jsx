e5dc11f2c2be28f1876e51e8632696e3db16b606
Abutalif Khan <talifkhan1@hotmail.com>
2025-04-12 15:42:53 +0530

    update - desktop chat page ui

--- src/pages/chat/ChatPage.jsx
+++ src/pages/chat/ChatPage.jsx
@@ -4,37 +4,61 @@ import { useOutletContext } from "react-router-dom";
 import useFetchAllChats from "../../hooks/data/useFetchAllChats";
 import Loader from "../../components/ui/loader/Loader";
 import ChatItem from "../../components/chat/ChatItem";
+import useDeviceType from "../../hooks/useDeviceType";
+import Chat_Illustration from "../../assets/chat_illus.svg";
+import { useState } from "react";
+import SingleChatPage from "./SingleChatPage";
+import { ChatState } from "../../contexts/chatContext";
 const ChatPage = () => {
 	const { setNavVisible } = useOutletContext();
 	const { chatData, chatDataError, chatDataLoading } = useFetchAllChats();
+	const [isChatSelected, setIsChatSelected] = useState(false);
+	const { selectedChat, setSelectedChat } = ChatState();
+	const deviceType = useDeviceType();
+	console.log(selectedChat);
 
 	useEffect(() => {
-		setNavVisible(false);
+		setNavVisible(deviceType === "Desktop");
 		return () => setNavVisible(true);
-	}, [setNavVisible]);
+	}, [setNavVisible, deviceType]);
 
 	return (
-		<main className="flex flex-col flex-1">
-			<ChatPageHeader />
-			<section className="flex-1 overflow-hidden">
-				{chatDataLoading ? (
-					<div className="grid size-full place-items-center">
-						<span className="size-[70px]">
-							<Loader />
-						</span>
-					</div>
-				) : chatDataError ? (
-					<div className="grid size-full place-items-center">
-						<p className="text-muted">{chatDataError}</p>
-					</div>
-				) : (
-					<div className="flex flex-col px-1 py-2 overflow-x-hidden overflow-y-auto size-full gap-y-1">
-						{chatData.map((chat) => (
-							<ChatItem {...chat} key={chat._id} />
-						))}
-					</div>
-				)}
-			</section>
+		<main className="flex flex-1">
+			<div className="flex flex-col flex-1 md:border-r-2 md:border-muted max-w-[425px]">
+				<ChatPageHeader />
+				<section className="flex-1 overflow-hidden">
+					{chatDataLoading ? (
+						<div className="grid size-full place-items-center">
+							<span className="size-[70px]">
+								<Loader />
+							</span>
+						</div>
+					) : chatDataError ? (
+						<div className="grid size-full place-items-center">
+							<p className="text-muted">{chatDataError}</p>
+						</div>
+					) : (
+						<div className="flex flex-col px-1 py-2 overflow-x-hidden overflow-y-auto size-full gap-y-1">
+							{chatData.map((chat) => (
+								<ChatItem
+									chatItemData={chat}
+									setIsChatSelected={setIsChatSelected}
+									key={chat._id}
+								/>
+							))}
+						</div>
+					)}
+				</section>
+			</div>
+			{deviceType === "Desktop" && (
+				<div className="grid flex-1 place-items-center">
+					{!selectedChat ? (
+						<img src={Chat_Illustration} alt="" />
+					) : (
+						<SingleChatPage chatIdProp={selectedChat._id} />
+					)}
+				</div>
+			)}
 		</main>
 	);
 };
