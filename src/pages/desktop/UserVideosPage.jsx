d02b04e8db5458ffac0f671ee5e4458c3d0cc2f5
Abutalif Khan <talifkhan1@hotmail.com>
2025-05-12 03:05:44 +0530

    update - user videos

--- src/pages/desktop/UserVideosPage.jsx
+++ src/pages/desktop/UserVideosPage.jsx
@@ -1,17 +1,31 @@
+import { useOutletContext } from "react-router-dom";
 import DeviceAccessDenied from "../../components/shared/DeviceAccessDenied";
-import useGetUserAllVideos from "../../hooks/data/useGetUserAllVideos";
+import ChannelVideos from "../../components/video/ChannelVideos";
 import useDeviceType from "../../hooks/useDeviceType";
 import { getUserData } from "../../services/authServices";
+import { useEffect } from "react";
 
 const UserVideosPage = () => {
+	const { setNavVisible } = useOutletContext();
 	const { username } = getUserData();
-	const { data, error, loading } = useGetUserAllVideos(username);
 	const deviceType = useDeviceType();
+
+	useEffect(() => {
+		setNavVisible(true);
+
+		return () => {
+			setNavVisible(true);
+		};
+	});
 
 	if (!deviceType === "Desktop") {
 		return <DeviceAccessDenied type="Desktop" />;
 	}
-	return <main></main>;
+	return (
+		<main>
+			<ChannelVideos username={username} /> 
+		</main>
+	);
 };
 
 export default UserVideosPage;
