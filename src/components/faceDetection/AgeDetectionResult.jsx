466604102c331d2362cf002697a8bc9037e45a3f
Abutalif Khan <talifkhan1@hotmail.com>
2025-05-14 07:02:00 +0530

    add - age detection comp

---
+++ src/components/faceDetection/AgeDetectionResult.jsx
@@ -0,0 +1,11 @@
+const AgeDetectionResult = ({ result }) => {
+	if (!result) return null;
+
+	return (
+		<div className="p-4 mt-6 text-center bg-green-100 shadow rounded-xl">
+			<p className="font-semibold">Success</p>
+		</div>
+	);
+};
+
+export default AgeDetectionResult;
