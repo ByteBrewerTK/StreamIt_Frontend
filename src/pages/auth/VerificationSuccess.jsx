aca1828c31041dd68f5a7fc38b43a3ca34251e8c
Abutalif Khan <talifkhan1@hotmail.com>
2025-05-14 07:11:32 +0530

    update - email verification with age verification in same place

--- src/pages/auth/VerificationSuccess.jsx
+++ src/pages/auth/VerificationSuccess.jsx
@@ -1,62 +1,74 @@
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { apiInstance } from "../../services/api";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
-import axios from "axios";
 import ScreenLoading from "../../components/ui/ScreenLoading";
 import CTAButton from "../../components/ui/CTAButton";
+import AgeDetectionPage from "../AgeDetectionPage";
+import { useNavigate } from "react-router-dom";
 
 const VerificationSuccess = () => {
 	const { pathname, search } = useLocation();
 	const [error, setError] = useState("");
 	const [isLoading, setLoading] = useState(false);
+	const [isAgeVerified, setIsAgeVerified] = useState(false);
+	const navigate = useNavigate();
+	const params = new URLSearchParams(search);
+	const email = params.get("e");
 
-	useEffect(() => {
+	const confirmationToken = `${pathname.split("/").at(-1)}${search}`;
+	const verifyEmail = async () => {
 		setLoading(true);
-		const source = axios.CancelToken.source();
-		const verifyEmail = async () => {
-			try {
-				await apiInstance.patch(
-					`/user/confirm/${pathname.split("/").at(-1)}${search}`
-				);
-				setLoading(false);
-			} catch (error) {
-				setError("Error occurred while verifying email");
-				console.log("Error occurred while verifying email : ", error);
-				setLoading(false);
-			}
-		};
-		verifyEmail();
-		return () => {
-			source.cancel("Request terminated: Component unmounted.");
+		try {
+			await apiInstance.patch(
+				`/user/confirm/${pathname.split("/").at(-1)}${search}`
+			);
+			navigate(`/auth/login?email=${email}`, { replace: true });
+		} catch (error) {
+			setError("Error occurred while verifying email");
+			console.log("Error occurred while verifying email : ", error);
+		} finally {
 			setLoading(false);
-		};
-	}, []);
+		}
+	};
+
+	const onSkipConfirmed = () => {
+		verifyEmail();
+	};
 
 	if (isLoading) return <ScreenLoading />;
 	return (
 		<div className="flex items-center justify-center min-h-screen bg-gray-100">
-			<div className="max-w-md p-8 text-center bg-white rounded-lg shadow-lg">
-				<h1 className="mb-4 text-2xl font-bold text-black">
-					{!error
-						? "Verification Successful!"
-						: "Verification Failed!"}
-				</h1>
-				<p className="mb-6 text-gray-700">
-					{!error
-						? "Your email has been successfully verified. You can now login to your account."
-						: "Your email verification was unsuccessful. Please try again later."}
-				</p>
+			{!isAgeVerified ? (
+				<AgeDetectionPage
+					setIsAgeVerified={setIsAgeVerified}
+					onSkipConfirmed={onSkipConfirmed}
+					confirmationToken={confirmationToken}
+					verifyEmail={verifyEmail}
+					email={email}
+				/>
+			) : (
+				<div className="max-w-md p-8 text-center bg-white rounded-lg shadow-lg">
+					<h1 className="mb-4 text-2xl font-bold text-black">
+						{!error
+							? "Verification Successful!"
+							: "Verification Failed!"}
+					</h1>
+					<p className="mb-6 text-gray-700">
+						{!error
+							? "Your email has been successfully verified. You can now login to your account."
+							: "Your email verification was unsuccessful. Please try again later."}
+					</p>
 
-				{!error ? (
-					<Link to={"/auth/login"}>
-						<CTAButton>Go to Login</CTAButton>
-					</Link>
-				) : (
-					<p className="text-red-500">{error}</p>
-				)}
-			</div>
+					{!error ? (
+						<Link to={"/auth/login"}>
+							<CTAButton>Go to Login</CTAButton>
+						</Link>
+					) : (
+						<p className="text-red-500">{error}</p>
+					)}
+				</div>
+			)}
 		</div>
 	);
 };
