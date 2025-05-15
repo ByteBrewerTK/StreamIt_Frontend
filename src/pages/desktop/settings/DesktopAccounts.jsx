fb21d7293eed1a55a7caf2029390a13d1d8dd9a9
Abutalif Khan <talifkhan1@hotmail.com>
2025-05-16 01:32:36 +0530

    update - add new fields and face verification

--- src/pages/desktop/settings/DesktopAccounts.jsx
+++ src/pages/desktop/settings/DesktopAccounts.jsx
@@ -7,12 +7,14 @@ import Loader from "../../../components/ui/loader/Loader";
 import { useContext } from "react";
 import { UserContext } from "../../../contexts/userContext";
 import { FaCheckCircle } from "react-icons/fa";
-import { IoMdCloseCircle } from "react-icons/io";
+import { IoMdClose, IoMdCloseCircle } from "react-icons/io";
 import axios from "axios";
 import DeviceAccessDenied from "../../../components/shared/DeviceAccessDenied";
 import useDeviceType from "../../../hooks/useDeviceType";
 import ImageCropper from "../../../components/ui/ImageCropper";
 import ProgressBar from "../../../components/ui/ProgressBar";
+import { Pencil } from "lucide-react";
+import AgeDetectionPage from "../../AgeDetectionPage";
 
 const Account = () => {
 	const deviceType = useDeviceType();
@@ -24,9 +26,15 @@ const Account = () => {
 	const [checkLoading, setCheckLoading] = useState(false);
 	const [uploadProgressActive, setUploadProgressActive] = useState(false);
 	const [uploadProgress, setUploadProgress] = useState(0);
+	const [isFaceDetectionModalOpen, setFaceDetectionModalOpen] =
+		useState(false);
+	const [isAgeVerified, setIsAgeVerified] = useState(false);
+
 	const [infoFormData, setInfoFormData] = useState({
-		fullName: userData.fullName,
-		username: userData.username,
+		fullName: userData.fullName || "",
+		username: userData.username || "",
+		age: userData.age || "",
+		gender: userData.gender || "",
 	});
 
 	useSetNavTitle("Account");
@@ -55,8 +63,13 @@ const Account = () => {
 	};
 
 	const submitHandler = async () => {
-		const { fullName } = userData;
-		if (!isUsernameAvailable && infoFormData.fullName === fullName) {
+		const { fullName, username, age, gender } = userData;
+		if (
+			!isUsernameAvailable &&
+			infoFormData.fullName === fullName &&
+			infoFormData.age === age &&
+			infoFormData.gender === gender
+		) {
 			return;
 		}
 		setUpdateLoading(true);
@@ -71,9 +84,10 @@ const Account = () => {
 			setUserData(response.data);
 			setUsernameAvailable(false);
 
-			toast.success("Account details updated");
+			toast.success("Account details updated successfully");
 		} catch (error) {
 			console.log(error);
+			toast.error("Failed to update account details");
 		} finally {
 			setUpdateLoading(false);
 		}
@@ -123,12 +137,50 @@ const Account = () => {
 			[target.name]: target.value,
 		}));
 	};
+	const onSkipConfirmed = () => {
+		setFaceDetectionModalOpen(false);
+	};
+
+	const isFormChanged = () => {
+		return (
+			infoFormData.fullName !== userData.fullName ||
+			isUsernameAvailable ||
+			infoFormData.age !== userData.age ||
+			infoFormData.gender !== userData.gender
+		);
+	};
+	const handleAfterVerification = (data) => {
+		setInfoFormData((prev) => ({
+			...prev,
+			age: data.age,
+			gender: data.gender,
+		}));
+	}
 
 	if (!deviceType === "Desktop") {
 		return <DeviceAccessDenied type="Desktop" />;
 	}
 	return (
-		<main className="relative flex flex-col flex-1 text-white ">
+		<main className="relative flex flex-col flex-1 text-white bg-secondary bg-opacity-90">
+			{isFaceDetectionModalOpen && (
+				<section className="absolute z-50 grid w-full h-full backdrop-blur-md place-items-center">
+					<div className="w-fit ">
+						<button
+							onClick={() => {
+								setFaceDetectionModalOpen(false);
+							}}
+							className="absolute translate-x-[50%] right-[50%] -translate-y-16 p-2 text-black bg-white rounded-full text-2xl"
+						>
+							<IoMdClose />
+						</button>
+						<AgeDetectionPage
+							setIsAgeVerified={setIsAgeVerified}
+							onSkipConfirmed={onSkipConfirmed}
+							handleAfterVerification={handleAfterVerification}
+						/>
+					</div>
+				</section>
+			)}
 			{uploadProgressActive && (
 				<ProgressBar uploadProgress={uploadProgress} />
 			)}
@@ -139,87 +191,142 @@ const Account = () => {
 				/>
 			)}
 			<form
-				onSubmit={submitHandler}
-				className="flex flex-col items-center py-4 bg-primary"
+				onSubmit={(e) => {
+					e.preventDefault();
+					submitHandler();
+				}}
+				className="flex flex-col items-center py-6 bg-primary"
 			>
 				<div
 					onClick={() => {
 						setAvatarPopupActive(true);
 					}}
-					className="size-[80px] rounded-full overflow-hidden mb-2 relative"
+					className="size-[100px] rounded-full overflow-hidden mb-3 relative border-2 border-white shadow-md hover:opacity-90 transition-opacity cursor-pointer"
 				>
-					<img src={userData.avatar} alt="" />
-					<span className="absolute bottom-0 flex justify-center w-full p-1 mx-auto bg-black h-fit bg-opacity-40 ">
+					<img
+						src={userData.avatar}
+						alt="Profile"
+						className="object-cover size-full"
+					/>
+					<span className="absolute bottom-0 flex justify-center w-full p-1 mx-auto bg-black h-fit bg-opacity-60">
 						<LuCamera className="text-lg text-white" />
 					</span>
 					{avatarUploading && (
 						<div className="absolute inset-0 grid m-auto bg-black bg-opacity-60 place-items-center size-full">
-							<span className=" size-8">
+							<span className="size-8">
 								<Loader />
 							</span>
 						</div>
 					)}
 				</div>
-				<span>{userData.fullName}</span>
+				<span className="text-lg font-medium">{userData.fullName}</span>
+				{userData.username && (
+					<span className="text-sm text-muted">
+						@{userData.username}
+					</span>
+				)}
 			</form>
-			<section className="flex flex-col justify-between flex-1 p-4 gap-y-2 ">
-				<div>
-					<label className="">
-						<span className="text-muted">Name</span>
-						<input
-							type="text"
-							name="fullName"
-							onChange={inputChangeHandler}
-							className="w-full p-2 rounded-md outline-none bg-primary focus:outline-muted_border outline-1 text-muted_dark"
-							defaultValue={userData.fullName}
-						/>
-					</label>
-					<label>
-						<span className="text-muted">Username</span>
-						<div className="relative flex">
+			<section className="flex flex-col justify-between flex-1 w-full max-w-md p-4 mx-auto gap-y-4">
+				<div className="space-y-4">
+					<div className="p-4 shadow-md bg-primary rounded-xl">
+						<label className="block mb-3">
+							<span className="text-muted font-medium block mb-1.5">
+								Name
+							</span>
 							<input
 								type="text"
-								name="username"
+								name="fullName"
 								onChange={inputChangeHandler}
-								onInput={checkAvailable}
-								className="w-full p-2 rounded-md outline-none bg-primary focus:outline-muted_border outline-1 text-muted_dark"
-								defaultValue={userData.username}
-								maxLength={20}
+								className="w-full p-2.5 rounded-lg outline-none bg-primary border border-muted_border focus:border-white transition-colors text-muted_dark"
+								defaultValue={userData.fullName}
 							/>
-							{!(userData.username === infoFormData.username) &&
-								infoFormData.username && (
-									<span className="absolute top-[50%] -translate-y-[50%] right-2">
-										{isUsernameAvailable ? (
-											<FaCheckCircle className="text-green-500" />
-										) : (
-											<IoMdCloseCircle className="text-red-500" />
-										)}
-									</span>
-								)}
+						</label>
+						<label className="block">
+							<span className="text-muted font-medium block mb-1.5">
+								Username
+							</span>
+							<div className="relative flex">
+								<input
+									type="text"
+									name="username"
+									onChange={inputChangeHandler}
+									onInput={checkAvailable}
+									className="w-full p-2.5 rounded-lg outline-none bg-primary border border-muted_border focus:border-white transition-colors text-muted_dark"
+									defaultValue={userData.username}
+									maxLength={20}
+								/>
+								{!(
+									userData.username === infoFormData.username
+								) &&
+									infoFormData.username && (
+										<span className="absolute top-[50%] -translate-y-[50%] right-2">
+											{isUsernameAvailable ? (
+												<FaCheckCircle className="text-green-500" />
+											) : (
+												<IoMdCloseCircle className="text-red-500" />
+											)}
+										</span>
+									)}
+							</div>
+						</label>
+					</div>
+
+					<div className="p-4 shadow-md bg-primary rounded-xl">
+						<div className="flex justify-between">
+							<h3 className="mb-3 text-base font-medium text-white">
+								Personal Information
+							</h3>
+							<button
+								onClick={() => setFaceDetectionModalOpen(true)}
+								className="text-white top-4 right-4 hover:text-white"
+								aria-label="Edit Personal Info"
+							>
+								<Pencil className="w-5 h-5" />
+							</button>
 						</div>
-					</label>
+						<div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+							<label className="block">
+								<span className="text-muted font-medium block mb-1.5">
+									Age Group
+								</span>
+								<div className="w-full p-2.5 rounded-lg outline-none bg-primary border border-muted_border focus:border-white transition-colors text-muted_dark">
+									<span>{userData.age}</span>
+								</div>
+							</label>
+							<label className="block">
+								<span className="text-muted font-medium block mb-1.5">
+									Gender
+								</span>
+								<div
+									name="gender"
+									onChange={inputChangeHandler}
+									className="w-full p-2.5 rounded-lg outline-none bg-primary border border-muted_border focus:border-white transition-colors text-muted_dark"
+								>
+									<span>{userData.gender}</span>
+								</div>
+							</label>
+						</div>
+					</div>
 				</div>
-				{infoFormData.fullName !== userData.fullName ||
-				isUsernameAvailable ||
-				updateLoading ? (
+				{isFormChanged() || updateLoading ? (
 					<button
 						onClick={submitHandler}
-						className="flex items-center justify-center h-8 py-1 font-semibold text-black bg-white rounded-full"
-					>
-						Update
-					</button>
-				) : (
-					<button
-						className="flex items-center justify-center h-8 py-1 font-semibold text-black bg-white rounded-full disabled:bg-opacity-50"
-						disabled
+						className="flex items-center justify-center py-2.5 font-semibold text-black bg-white rounded-full hover:bg-opacity-90 transition-all"
 					>
 						{updateLoading ? (
-							<span className="size-4">
+							<span className="mr-1 size-5">
 								<Loader />
 							</span>
 						) : (
-							"Update"
+							"Save Changes"
 						)}
+					</button>
+				) : (
+					<button
+						className="flex items-center justify-center py-2.5 font-semibold text-black bg-white rounded-full disabled:bg-opacity-50 transition-all"
+						disabled
+					>
+						Save Changes
 					</button>
 				)}
 			</section>
