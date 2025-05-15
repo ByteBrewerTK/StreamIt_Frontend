8bd5444b3a76cb7f748f706c63c8a765c794cc20
Abutalif Khan <talifkhan1@hotmail.com>
2025-05-16 01:46:07 +0530

    fix - ui contrast

--- src/components/faceDetection/AgeDetectionResult.jsx
+++ src/components/faceDetection/AgeDetectionResult.jsx
@@ -3,7 +3,7 @@ const AgeDetectionResult = ({ result }) => {
 
 	return (
 		<div className="p-4 mt-6 text-center bg-green-100 shadow rounded-xl">
-			<p className="font-semibold">Success</p>
+			<p className="font-semibold text-black">Success</p>
 		</div>
 	);
 };
