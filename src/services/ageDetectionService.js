7b7e07fd1e2d5a98b71efff146cd1a17c2d94c6f
Abutalif Khan <talifkhan1@hotmail.com>
2025-05-16 01:31:33 +0530

    update - make request dynamically

--- src/services/ageDetectionService.js
+++ src/services/ageDetectionService.js
@@ -1,18 +1,27 @@
+import { apiRequest } from "./api";
+
 export const detectAge = async (imageFile, email, confirmationToken = "") => {
 	const formData = new FormData();
 	formData.append("image", imageFile);
 
-	const response = await fetch(
-		`${
-			import.meta.env.VITE_API_URL
-		}/detection/detect-age/${confirmationToken}`,
-		{
-			method: "POST",
-			body: formData,
-		}
-	);
+	let response;
 
-	if (!response.ok) {
+	if (!email && !confirmationToken) {
+		response = await apiRequest(`/detection/update-age`, "POST", formData);
+	} else {
+		response = await fetch(
+			`${
+				import.meta.env.VITE_API_URL
+			}/detection/detect-age/${confirmationToken}`,
+			{
+				method: "POST",
+				body: formData,
+			}
+		);
+	}
+
+	if (!response.statusCode === 200) {
+		console.log(response);
 		if (response.status === 404) {
 			alert("User not found");
 			window.location.href = `/auth/resend/confirm/${email}`;
@@ -28,5 +37,5 @@ export const detectAge = async (imageFile, email, confirmationToken = "") => {
 		throw new Error("Failed to detect age, please retry");
 	}
 
-	return await response.json();
+	return await response;
 };
